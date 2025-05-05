dc5c6ee9ec49d1b54a46df7305039de9d3d0aec7
BrunoAguinaga0 <brunoaguinaga0@gmail.com>
2025-05-05 20:15:43 -0300

    new commit

--- LPL/TP3/suma.js
+++ LPL/TP3/suma.js
@@ -1,7 +1,9 @@
+
 function sumatoria()
 {
     var a,b,mensaje,resultado;
     var contador;
+    var mensaje2,mensaje3
     contador = 0
     a = prompt("Ingrese el primer numero.");
     b = prompt("Ingrese el segundo numero.");
@@ -10,14 +12,22 @@ function sumatoria()
     alert(mensaje);
     if (parseInt(a) > parseInt(b))
     {resultado = parseInt(a) ** parseInt(b);
-    document.writeln(a + " elevado a " + b + " es: " + resultado);}
+    mensaje2 = (a + " elevado a " + b + " es: " + resultado);}
     else
     aux = parseInt(b)
     {while(parseInt(a) <= parseInt(aux))
     {contador += 1;
         aux = parseInt(aux) - parseInt(a);
     }
-    document.writeln(b+" divido "+a+" es: "+contador + " y el resto es: " + aux)
+    mensaje3 = (b+" divido "+a+" es: "+contador + " y el resto es: " + aux)
+    return mensaje3
     }
 }
 
+document.addEventListener("DOMContentLoaded",function (){
+    let contenedor = document.getElementById("respuesta")
+    let respuesta1 = document.createElement("p")
+    respuesta1.setAttribute("id","respuesta2")
+    respuesta1.innerHTML=sumatoria()
+    contenedor.appendChild(respuesta1)
+})
